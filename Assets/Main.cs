60693e2638dec8b6f76138e1c5aa58afb61f3e08
Vedal987 <vedal876@gmail.com>
2017-01-26 18:26:37 +0000

    Moving

---
+++ Assets/Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	public bool inBattle;
+	public GameObject creature;
+
+	public float playerSpeed = 4f;
+
+	void Start () {
+
+	}
+
+	void FixedUpdate () {
+		Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		GetComponent<Rigidbody2D>().velocity=targetVelocity * playerSpeed;
+	}
+
+
+}
+
+
+
