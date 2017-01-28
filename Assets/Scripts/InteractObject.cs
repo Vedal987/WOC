00298ae9e6b29659530895da81487edb5bfbcd75
Vedal987 <vedal876@gmail.com>
2017-01-28 10:35:19 +0000

    IdleAnimation

---
+++ Assets/Scripts/InteractObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractObject : MonoBehaviour {
+
+	public string[] Dialogue;
+	private int d = 0;
+
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
 
-	public bool inBattle;
+	public bool inBattle = false;
 	public GameObject creature;
 	public GameObject BattleUI;
 	public GameObject model;
+	public GameObject DialogueBox;
+	public GameObject DialogueText;
 	public Animator modelAnimator;
 	float playerSpeed = 4f;
+
+	public string LastKeyPress;
+
 
 	void Start () {
 		modelAnimator = model.GetComponent<Animator> ();
@@ -19,10 +24,55 @@ public class Main : MonoBehaviour {
 		
 	void Update()
 	{
-		float hor = Input.GetAxisRaw ("Horizontal");
-		float ver = Input.GetAxisRaw ("Vertical");
-		modelAnimator.SetFloat ("Horizontal", hor);
-		modelAnimator.SetFloat ("Vertical", ver);
+		if (!inBattle) {
+			float hor = Input.GetAxisRaw ("Horizontal");
+			float ver = Input.GetAxisRaw ("Vertical");
+			modelAnimator.SetFloat ("Horizontal", hor);
+			modelAnimator.SetFloat ("Vertical", ver);
+			if (Input.GetKeyUp (KeyCode.W)) {
+				LastKeyPress = "W";
+			}
+			if (Input.GetKeyUp (KeyCode.A)) {
+				LastKeyPress = "A";
+			}
+			if (Input.GetKeyUp (KeyCode.S)) {
+				LastKeyPress = "S";
+			}
+			if (Input.GetKeyUp (KeyCode.D)) {
+				LastKeyPress = "D";
+			}
+			if (!Input.anyKey) {
+				if (LastKeyPress == "W") {
+					modelAnimator.SetBool ("IdleUp", true);
+					modelAnimator.SetBool ("IdleRight", false);
+					modelAnimator.SetBool ("IdleDown", false);
+					modelAnimator.SetBool ("IdleLeft", false);
+				}
+				if (LastKeyPress == "A") {
+					modelAnimator.SetBool ("IdleLeft", true);
+					modelAnimator.SetBool ("IdleRight", false);
+					modelAnimator.SetBool ("IdleDown", false);
+					modelAnimator.SetBool ("IdleUp", false);
+				}
+				if (LastKeyPress == "S") {
+					modelAnimator.SetBool ("IdleDown", true);
+					modelAnimator.SetBool ("IdleRight", false);
+					modelAnimator.SetBool ("IdleLeft", false);
+					modelAnimator.SetBool ("IdleUp", false);
+				}
+				if (LastKeyPress == "D") {
+					modelAnimator.SetBool ("IdleRight", true);
+					modelAnimator.SetBool ("IdleDown", false);
+					modelAnimator.SetBool ("IdleLeft", false);
+					modelAnimator.SetBool ("IdleUp", false);
+				}
+			} else {
+				modelAnimator.SetBool ("IdleRight", false);
+				modelAnimator.SetBool ("IdleDown", false);
+				modelAnimator.SetBool ("IdleLeft", false);
+				modelAnimator.SetBool ("IdleUp", false);
+			}
+		}
 	}
 
 	void FixedUpdate () {
