4d510cfb81a08fd6c6bc6e0af1e1f6d20fb70c47
Vedal987 <vedal876@gmail.com>
2017-05-06 20:40:13 +0100

    0.8.6.0

---
+++ Assets/Save.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour {
+
+	public string[] Dialogue;
+	public int d = 0;
+	private GameObject player;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	public void Interact()
+	{
+		if (d == Dialogue.Length) {
+			player.GetComponent<Main> ().Dialogue ("x7Finish");
+			return;
+		}
+		string dia = Dialogue [d];
+		d++;
+		player.GetComponent<Main> ().Dialogue (dia);
+	}
+
+}
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicRoom : MonoBehaviour {
+public class Room : MonoBehaviour {
 
 	public AudioClip music;
 
